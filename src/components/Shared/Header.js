dede0fd24ec18f2c452afdb860e35c921205de4b
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-07-05 05:38:32 +0600

    profile updated

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -5,6 +5,7 @@ import logo from '../../assets/library_logo.png'
 import auth from '../../firebase.init';
 import { Link, useNavigate } from 'react-router-dom';
 import { GoVerified } from 'react-icons/go';
+import { HiUserCircle } from 'react-icons/hi';
 import { RiLogoutBoxRLine, RiLogoutBoxLine } from 'react-icons/ri';
 
 
@@ -20,14 +21,14 @@ const Header = () => {
             <div className=''>
                 <img className='w-32' src={logo} alt="brand_logo" />
             </div>
-                <h1 className='text-center font-semibold uppercase text-xs lg:text-4xl flex-grow font-mono px-2'><span className='text-yellow-600'>🅻🅸🅱🆁🅰🆁🆈</span> <span className='text-pink-500'>🅼🅰🅽🅰🅶🅴🅼🅴🅽🆃</span></h1>
+                <h1 className='text-center font-semibold uppercase text-xs lg:text-4xl flex-grow font-mono px-2 lg:px-0'><span className='text-yellow-600'>🅻🅸🅱🆁🅰🆁🆈</span> <span className='text-pink-500'>🅼🅰🅽🅰🅶🅴🅼🅴🅽🆃</span></h1>
             <div className='flex justify-end gap-x-4'>
                 {/* Responsive Menu */}
-                {!user ? <Link className='lg:hidden border border-accent px-1 w-8 rounded-md bg-slate-600 hover:bg-transparent hover:text-white shadow-lg font-bold text-white flex items-center gap-x-2' to="/signup"><GoVerified size="large"/></Link> : ""}
+                {!user ? <Link className='lg:hidden border border-accent px-1 w-8 rounded-md bg-slate-600 hover:bg-transparent hover:text-white shadow-lg font-bold text-white flex items-center gap-x-2' to="/signup"><GoVerified size="large"/></Link> : <Link className='lg:hidden border-none w-8 rounded-md bg-slate-600 hover:bg-transparent hover:text-white shadow-lg font-bold text-white flex items-center gap-x-2' to="/profile"><HiUserCircle size="large"/></Link>}
                 {user ? <button className='lg:hidden border px-1 w-8 py-2 rounded-md hover:bg-slate-600 shadow-lg font-bold text-white flex items-center gap-x-2' onClick={logOut}><RiLogoutBoxRLine size="large"/></button> : <Link className='lg:hidden border px-1 w-8 py-2 rounded-md hover:bg-slate-600 shadow-lg font-bold text-white flex items-center gap-x-2' to="/signin"><RiLogoutBoxLine size="large"/></Link>}
                 {/* Responsive Menu */}
 
-                <Link className='hidden border border-accent px-5 py-2 rounded-md bg-slate-600 hover:bg-transparent hover:text-white shadow-lg font-bold text-white lg:flex items-center gap-x-2' to="/signup"><GoVerified /> SignUp</Link>
+                {!user ? <Link className='hidden border border-accent px-5 py-2 rounded-md bg-slate-600 hover:bg-transparent hover:text-white shadow-lg font-bold text-white lg:flex items-center gap-x-2' to="/signup"><GoVerified /> SignUp</Link> : <Link className='hidden border-none px-2 rounded-md bg-slate-600 hover:bg-transparent hover:text-white shadow-lg font-bold text-white lg:flex items-center gap-x-2' to="/profile"><HiUserCircle />{user?.displayName}</Link>}
                 {user ? <button className='hidden border px-5 py-2 rounded-md hover:bg-slate-600 shadow-lg font-bold text-white lg:flex items-center gap-x-2' onClick={logOut}><RiLogoutBoxRLine /> Logout</button> : <Link className='hidden border px-5 py-2 rounded-md hover:bg-slate-600  shadow-lg font-bold text-white lg:flex items-center gap-x-2' to="/signin"><RiLogoutBoxLine /> Login</Link>}
             </div>
         </div>
